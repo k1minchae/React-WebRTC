9a16c9e1b038680928a090effae5bb3c4b6b229d
김민채 <alsco9707@gmail.com>
2024-07-04 22:41:19 +0900

    ⚡️ React-study : 2일차 style 적용 (tailwind)

--- React/todo-app/src/components/TodoList.js
+++ React/todo-app/src/components/TodoList.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 function TodoList() {
   const [today, setToday] = useState('');
+  const tasks = [
+    'React 문서 읽기',
+    'Tailwind CSS 연습하기',
+    '스터디 과제 완료하기',
+    '알고리즘 문제 풀이',
+    'GitHub에 코드 푸시하기',
+  ];
 
   const currentDate = () => {
     const date = new Date();
@@ -15,22 +22,20 @@ function TodoList() {
     currentDate();
   }, []);
 
-  const todos = [
-    'React 문서 읽기',
-    'Tailwind CSS 연습하기',
-    '스터디 과제 완료하기',
-    '알고리즘 문제 풀이',
-    'GitHub에 코드 푸시하기',
-  ];
-
   return (
-    <div className="my-8">
+    <div className="my-12">
       <h1 className="font-['Ownglyph'] text-2xl text-center">{today}</h1>
-      <ul className="my-3 w-[50vh]">
-        {todos.map((task, index) => (
-          <li key={index} className='font-["Ownglyph"] text-xl mt-1'>
-            <input type="checkbox" className="mr-3" />
-            {task}
+      <ul className="my-4 w-[60vh]">
+        {tasks.map((task, index) => (
+          <li key={index} className='font-["Ownglyph"] text-xl flex items-center mt-1'>
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                className="form-checkbox h-4 w-4 text-green-500"
+                style={{ accentColor: 'green' }}
+              />
+              <span className="ml-2">{task}</span>
+            </label>
           </li>
         ))}
       </ul>
